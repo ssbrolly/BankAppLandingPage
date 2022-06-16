ec71938482a2f7265c8fabb747a5803989453077
Usong Cho <silvermouse4012@gmail.com>
2022-06-16 13:36:16 -0700

    implemented the slider function

--- script.js
+++ script.js
@@ -119,27 +119,6 @@ headerObserver.observe(header);
 ////////////////////////////////////////////////////
 //  Reveal Sections
 
-// const allSections = document.querySelectorAll('.section');
-
-// const revealSection = function (entries, observer) {
-// 	const [entry] = entries;
-
-// 	if (!entry.isIntersecting) return;
-
-// 	entry.target.classList.remove('section--hidden');
-// 	observer.unobserve(entry.target);
-// };
-
-// const sectionObserver = new IntersectionObserver(revealSection, {
-// 	root: null,
-// 	threshold: 0.15,
-// });
-
-// allSections.forEach(function (section) {
-// 	sectionObserver.observe(section);
-// 	section.classList.add('section--hidden');
-// });
-
 const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
@@ -156,7 +135,125 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 	threshold: 0.15,
 });
 
-allSections.forEach(section => {
-	section.classList.add('section--hidden');
+allSections.forEach(function (section) {
 	sectionObserver.observe(section);
+
+	// ///////////////// Do not forget
+	// section.classList.add('section--hidden');
 });
+
+////////////////////////////////////////////////////
+// Lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+	const [entry] = entries;
+
+	if (!entry.isIntersecting) return;
+
+	entry.target.src = entry.target.dataset.src;
+
+	entry.target.addEventListener('load', function () {
+		entry.target.classList.remove('lazy-img');
+	});
+
+	observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+	root: null,
+	threshold: 0,
+	rootMargin: '200px',
+});
+
+imgTargets.forEach(img => {
+	imgObserver.observe(img);
+});
+
+////////////////////////////////////////////////////
+// Sliders
+const slider = function () {
+	const slides = document.querySelectorAll('.slide');
+	const btnLeft = document.querySelector('.slider__btn--left');
+	const btnRight = document.querySelector('.slider__btn--right');
+	let curSlide = 0;
+	const maxSlide = slides.length;
+
+	const dotContainer = document.querySelector('.dots');
+
+	const createDots = function () {
+		slides.forEach((_, i) => {
+			dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`);
+		});
+	};
+
+	const activateDot = function (slide) {
+		document.querySelectorAll('.dots__dot').forEach(dot => {
+			dot.classList.remove('dots__dot--active');
+		});
+
+		document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+	};
+
+	const gotoSlide = function (slide) {
+		slides.forEach((s, i) => {
+			s.style.transform = `translateX(${100 * (i - slide)}%)`;
+		});
+	};
+
+	const nextSlide = function () {
+		if (curSlide === maxSlide - 1) {
+			curSlide = 0;
+		} else {
+			curSlide++;
+		}
+		gotoSlide(curSlide);
+
+		activateDot(curSlide);
+	};
+
+	const prevSlide = function () {
+		if (curSlide === 0) {
+			curSlide = maxSlide - 1;
+		} else {
+			curSlide--;
+		}
+
+		gotoSlide(curSlide);
+		activateDot(curSlide);
+	};
+
+	////////////////////////////////////////////////////
+	// Initializer
+
+	const init = function (curSlide) {
+		gotoSlide(curSlide);
+		createDots();
+		activateDot(curSlide);
+	};
+
+	init(curSlide);
+
+	////////////////////////////////////////////////////
+	// Event handlers
+	btnRight.addEventListener('click', nextSlide);
+	btnLeft.addEventListener('click', prevSlide);
+
+	document.addEventListener('keydown', function (e) {
+		if (e.key === 'ArrowLeft') {
+			prevSlide();
+		} else if (e.key === 'ArrowRight') {
+			nextSlide();
+		}
+	});
+
+	dotContainer.addEventListener('click', function (e) {
+		if (e.target.classList.contains('dots__dot')) {
+			const {slide} = e.target.dataset;
+			gotoSlide(slide);
+			activateDot(slide);
+		}
+	});
+};
+slider();
